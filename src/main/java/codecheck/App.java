f19be1e0ba1506580301a264494421e73307f52e
uchida <uchida.masato.633@tis.co.jp>
2018-06-15 20:26:30 +0900

    解答

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,40 @@
 package codecheck;
 
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		Map<BigInteger, String> fizzBuzzMap = new TreeMap<BigInteger, String>();
+		
+		for (int i = 0, l = args.length; i < l -1; i++) {
+			String inputFizzBuzz[] = args[i].split(":");
+			BigInteger a = new BigInteger(inputFizzBuzz[0]);
+			StringBuilder s = new StringBuilder();
+			for (int j = 1; j < inputFizzBuzz.length; j++) {
+				s.append(inputFizzBuzz[j]);
+			}
+			
+			fizzBuzzMap.put(a, s.toString());
 		}
+		
+		BigInteger m = new BigInteger(args[args.length -1]);
+		
+		StringBuilder answer = new StringBuilder();
+		
+		for (Map.Entry<BigInteger, String> fizzBuzz : fizzBuzzMap.entrySet()) {
+			BigInteger remainder = m.remainder(fizzBuzz.getKey());
+			
+			if (BigInteger.ZERO.equals(remainder)) {
+				answer.append(fizzBuzz.getValue());
+			}
+		}
+		
+		if (answer.length() == 0) {
+			answer.append(m.toString());
+		}
+		
+		System.out.println(answer.toString());
 	}
 }
